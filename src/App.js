abce3c14f90aea538dec20f35103e183d27c738c
KianaYazdi <kiananezafat6.kn@gmail.com>
2025-10-02 12:37:35 -0400

    lab05: React SPA renders locally with student info

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main style={{fontFamily:"system-ui, -apple-system, Segoe UI, Roboto", padding:"2rem"}}>
+      <h1>COMP3104 – DevOps SPA</h1>
+      <h2>Kiana Nezafat Yazdi — 101488042</h2>
+</main>
+  );
+}
